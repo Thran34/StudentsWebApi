c313ceddf405fdc770f2d11fba6618a7aef2e2a9
Andrzej Jasiński <thran34@interia.pl>
2022-06-30 23:02:12 +0200

    added mvc project and CORS configuration

--- StudentsWebApi/Controllers/StudentsController.cs
+++ StudentsWebApi/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentsWebApi.Domain.Model;
 using StudentsWebApi.Infrastructure.Context;
@@ -7,6 +8,7 @@ namespace StudentsWebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("MyAllowSpecificOrigins")]
     public class StudentsController : ControllerBase
     {
         private readonly DBContext _context;
