572c6890ca1d070425c90453267afeee2422c486
Andrzej Jasiński <thran34@interia.pl>
2022-07-08 22:36:52 +0200

    latest changes

--- StudentsWebApiDomain/Model/Teacher.cs
+++ StudentsWebApiDomain/Model/Teacher.cs
@@ -5,5 +5,6 @@
         public int TeacherId { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+        public string Subject { get; set; }
     }
 }
