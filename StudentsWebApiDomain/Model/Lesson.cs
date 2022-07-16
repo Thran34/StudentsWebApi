bb1881af195b7e44b0f36b9fcd516fed1cf0d758
Andrzej Jasiński <thran34@interia.pl>
2022-07-16 11:50:28 +0200

    update

--- StudentsWebApiDomain/Model/Lesson.cs
+++ StudentsWebApiDomain/Model/Lesson.cs
@@ -8,8 +8,7 @@ namespace StudentsWebApi.Domain.Model
         public string LessonSubject { get; set; }
         [JsonIgnore]
         public IEnumerable<Student>? Students { get; set; }
-        public string Test { get; set; }
-        public string Develop { get; set; }
+
 
     }
 }
